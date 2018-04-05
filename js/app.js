edf255879fe1ed72fca4d2037b0611d965d9247d
Verena Ena <verenaparaluxe@gmail.com>
2018-04-05 14:44:32 +0200

    fix: add click handler for each card

--- js/app.js
+++ js/app.js
@@ -47,3 +47,8 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+ // click handler event listener
+ $('.card').click(function() {
+   console.log('you made clicks on a card');
+ });

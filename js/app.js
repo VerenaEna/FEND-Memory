78a20d54e634f2b1dff6e9446dfb46137b66a25e
Verena Ena <verenaparaluxe@gmail.com>
2018-04-06 11:35:09 +0200

    fix: add click event listener for cards

--- js/app.js
+++ js/app.js
@@ -52,3 +52,8 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+ //event listener
+ $('.card').click(function(){
+   check(this);
+ });

bbd308c9fcc8bdd4a9666cff85ba903f874e861e
Verena Ena <verenaparaluxe@gmail.com>
2018-04-07 15:11:11 +0200

    fix: add reset and set start in html to click

--- js/app.js
+++ js/app.js
@@ -28,19 +28,28 @@ let second = 0;
 let minute = 0;
 var interval;
 
-$(document).ready(function(){
-
-
   // @description: function start game will shuffle and display each card
   function start(){
     cards = shuffle(cardsArray);
      // remove all default classes from each card on the deck
      for (let index of cardsArray){
-       deck.innerHTML = ''; // empty deck
+       deck.html = ''; // empty deck
        cardsArray.forEach(function(item){
-         deck.append(item);
+         card.append(item);
+       });
+       cardsArray.forEach(function(){
+         card.removeClass('show open match disabled');
        });
      }
+
+    //reset Moves
+    counter = 0;
+    moves.text(counter);
+    //reset star rating
+    for(let index in stars){
+      star = 3;
+      stars.css('visibility', 'visible');
+    };
     // set game timer / reset on reload
     $('.timer').text('0 minute 00 seconds');
     clearInterval(interval);
@@ -86,14 +95,13 @@ $(document).ready(function(){
   }
   //enable cards and disable matched cards
   function enable(){
-      deck.find('.disabled').removeClass('disabled');
-      for(let index of match){
-        match.forEach(function(){
-          deck.find('.unmatched').addClass('disabled');
-        })
-      };
+    deck.find('.disabled').removeClass('disabled');
+    for(let index of match){
+      match.forEach(function(){
+        deck.find('.unmatched').addClass('disabled');
+      })
+    };
   };
-
   //@description: the counter moves on each pair click
   //@description: star rating changes deppens on moves
   function moveCounter(){
@@ -107,13 +115,16 @@ $(document).ready(function(){
       startTimer();
     }
     //star rating
-    if (counter > 28 && counter < 27){
-          stars.eq(2).css("visibility","collapse");
-    } else if(counter > 30){
-          stars.eq(1).css("visibility","collapse");
+    if (counter < 20 ){
+      star = 3;
+    } else if (counter > 21 && counter < 26){
+      star = 2;
+      stars.eq(2).css("visibility","collapse");
+    } else if (counter > 27){
+      star = 1
+      stars.eq(1).css("visibility","collapse");
     }
   }
-
   //@description: game timer runs proper
   function startTimer(){
     interval = setInterval(function(){
@@ -123,55 +134,48 @@ $(document).ready(function(){
         minute++;
         second = 0;
       }
-      //}
       if(second < 10){
         second = `0${second}`;
       }
     }, 1000);
   }
 
+  /*
+  * Display the cards on the page
+  *   - shuffle the list of cards using the provided "shuffle" method below
+  *   - loop through each card and create its HTML
+  *   - add each card's HTML to the page
+  */
+
+  // @description: shuffles cards (provided)
+  // Shuffle function from http://stackoverflow.com/a/2450976
+  function shuffle(array) {
+     var currentIndex = array.length, temporaryValue, randomIndex;
+     while (currentIndex !== 0) {
+         randomIndex = Math.floor(Math.random() * currentIndex);
+         currentIndex -= 1;
+         temporaryValue = array[currentIndex];
+         array[currentIndex] = array[randomIndex];
+         array[randomIndex] = temporaryValue;
+     }
+     return array;
+  }
 
   /*
-   * Display the cards on the page
-   *   - shuffle the list of cards using the provided "shuffle" method below
-   *   - loop through each card and create its HTML
-   *   - add each card's HTML to the page
-   */
+  * set up the event listener for a card. If a card is clicked:
+  *  - display the card's symbol (put this functionality in another function that you call from this one)
+  *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+  *  - if the list already has another card, check to see if the two cards match
+  *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+  *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+  */
 
+  //event listener
+  $('.card').click(function(){
+   check(this);
+   moves.text(counter);
+  });
 
-   // @description: shuffles cards (provided)
-   // Shuffle function from http://stackoverflow.com/a/2450976
-   function shuffle(array) {
-       var currentIndex = array.length, temporaryValue, randomIndex;
-
-       while (currentIndex !== 0) {
-           randomIndex = Math.floor(Math.random() * currentIndex);
-           currentIndex -= 1;
-           temporaryValue = array[currentIndex];
-           array[currentIndex] = array[randomIndex];
-           array[randomIndex] = temporaryValue;
-       }
-
-       return array;
-   }
-
-  /*
-   * set up the event listener for a card. If a card is clicked:
-   *  - display the card's symbol (put this functionality in another function that you call from this one)
-   *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
-   *  - if the list already has another card, check to see if the two cards match
-   *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
-   *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
-   *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
-   *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
-   */
-
-   //event listener
-   $('.card').click(function(){
-     check(this);
-     moves.text(counter);
-   });
-
-   start();
-
-});
+start();

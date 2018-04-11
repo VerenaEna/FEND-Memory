7238e837e6a5de409045e329ce6966a8935b4f12
Verena Ena <verenaparaluxe@gmail.com>
2018-04-11 10:05:41 +0200

    docs: add comments and make the file clean

--- js/app.js
+++ js/app.js
@@ -174,28 +174,20 @@ let againButton = $('#play-again');
     closeModal();
     playAgain();
   }
-
+  // for the close Icon in the winner Modal
   function closeModal(){
     close.click(function(e){
       gameEnd.removeClass("show");
       start();
     });
   }
-
+  // for the playAgain button in the winner modal
   function playAgain(){
     againButton.click(function(e){
       gameEnd.removeClass('show');
       start();
     });
   }
-
-
-  /*
-  * Display the cards on the page
-  *   - shuffle the list of cards using the provided "shuffle" method below
-  *   - loop through each card and create its HTML
-  *   - add each card's HTML to the page
-  */
 
   // @description: shuffles cards (provided)
   // Shuffle function from http://stackoverflow.com/a/2450976
@@ -210,17 +202,6 @@ let againButton = $('#play-again');
      }
      return array;
   }
-
-  /*
-  * set up the event listener for a card. If a card is clicked:
-  *  - display the card's symbol (put this functionality in another function that you call from this one)
-  *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
-  *  - if the list already has another card, check to see if the two cards match
-  *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
-  *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
-  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
-  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
-  */
 
   //event listener
   card.click(function(){

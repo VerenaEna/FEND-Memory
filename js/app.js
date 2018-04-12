967d770a5d5dff9ec6e82cb7d8b57c9eb79a3029
Verena Ena <verenaparaluxe@gmail.com>
2018-04-12 14:05:31 +0200

    Fix: bugfixes after Review

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ const cube = $(".fa .fa-cube");
 const leaf = $(".fa .fa-leaf");
 const bicycle = $(".fa .fa-bicycle");
 const bomb = $(".fa .fa-bomb");
-const card = $(".card");
+let card = $(".card");
 //create a list holds all icons
 let cardsArray = [...card];
 console.log(cardsArray);
@@ -30,15 +30,15 @@ let second = 0;
 let minute = 0;
 let interval;
 //define modal
-let modal = $("#modal");
+let modal = $('#modal');
 // define stars list
-let starsList = $(".stars li");
+let starsList = $('.stars li');
 // close icon for modal
-let close = $(".close");
+let close = $('.close');
 //define game end
-let gameEnd = $(".popup");
+let gameEnd = $('.popup');
 //define play playAgain
-let againButton = $("#play-again");
+let againButton = $('#play-again');
 
 // @description: function start game will shuffle and display each card
 function start() {
@@ -49,14 +49,15 @@ function start() {
     cards.forEach(function(item) {
       deck.append(item);
     });
-  }
+    matchedCards = [];
+  };
   resetGame();
+  startTimer();
+
 }
 //@description: reset Game function
 function resetGame() {
-  setTimeout(function() {
-    $(cards).removeClass("show open match disabled");
-  }, 100);
+  $(cards).removeClass('show open match disabled');
   //set/reset matchCards count
   matchCards = 0;
   //reset Moves
@@ -65,7 +66,7 @@ function resetGame() {
   //reset star rating
   for (let index in stars) {
     star = 3;
-    stars.css("visibility", "visible");
+    stars.css('visibility', 'visible');
   }
   // set game timer / reset on reload
   second = 0;
@@ -74,62 +75,52 @@ function resetGame() {
   clearInterval(interval);
 }
 
-//@description: check Cards if match
-function check(type) {
-  $(type).addClass("open show");
-  matchedCards.push(type);
+
+function check(element) {
+  // debug for more than 2 clicks
+  if ($(element).hasClass('show') || $(element).hasClass('match')) {
+    return true;
+  }
+  $(element).addClass('show open');
+  matchedCards.push(element);
   let length = matchedCards.length;
   if (length === 2) {
-    moveCounter();
+    counter++;
     if (matchedCards[0].type === matchedCards[1].type) {
       matched();
-      matchCards++;
-      console.log(type);
-      console.log("yeah, that was a match - keep on going :)");
+      console.log('yeah, that was a match - keep on going :)');
     } else {
       unmatched();
-      console.log("sorry, no match - try another pair ;)");
+      matchedCards = [];
+      console.log('sorry, no match - try another pair ;)');
     }
   }
 }
 //for if cards matching
 function matched() {
-  deck.find(".open").addClass("match disabled");
-  deck.find(".match").removeClass("show open");
-  disabled();
+  deck.find('.open').addClass('match');
+  setTimeout(function () {
+      deck.find('.match').removeClass('open');
+  }, 500);
+  matchCards++;
   matchedCards = [];
 }
 //for if cards not matching
 function unmatched() {
-  deck.find(".open").addClass("unmatched");
-  disabled();
-  setTimeout(function() {
-    deck.find(".unmatched").removeClass("show open unmatched");
-    enable();
-    matchedCards = [];
+  deck.find('.open').addClass('unmatched');
+  setTimeout(function () {
+      deck.find('.open').removeClass('open show unmatched');
+      matchedCards = [];
   }, 500);
 }
-//disable cards temporarly
-function disabled() {
-  deck.find(".match").addClass("disabled");
-  deck.find(".unmatched").addClass("disabled");
-}
-//enable cards and disable matched cards
-function enable() {
-  deck.find(".disabled").removeClass("disabled");
-  for (let index of match) {
-    match.forEach(function() {
-      deck.find(".unmatched").addClass("disabled");
-    });
-  }
-}
+
 //@description: the counter moves on each pair click
 //@description: star rating changes deppens on moves
-function moveCounter() {
+function moveCounter(){
   counter++;
   moves.text(counter);
   //start timer on first moves
-  if (counter == 1) {
+  if(counter == 1){
     second = 0;
     minute = 0;
     startTimer();
@@ -146,47 +137,45 @@ function moveCounter() {
   }
 }
 //@description: game timer runs proper
-function startTimer() {
-  interval = setInterval(function() {
-    timer.text(`${minute} minute ${second} seconds`);
-    second++;
-    if (second == 60) {
-      minute++;
-      second = 0;
-    }
-    if (second < 10) {
-      second = `0${second}`;
-    }
-  }, 1000);
-}
+function startTimer(){
+    interval = setInterval(function(){
+      timer.text(`${minute} minute ${second} seconds`);
+      second++;
+      if(second == 60){
+        minute++;
+        second = 0;
+      }
+      if(second < 10){
+        second = `0${second}`;
+      }
+    }, 1000);
+  }
 
 //@description: If player found all pairs a winner modal appears
 function winner() {
-  gameEnd.addClass("show");
-  const timerResult = timer.text();
-  let winnerMessage = $(".winner-message");
-  let starsIcon = starsList.html();
-  winnerMessage.html(
-    `<p class="winner-title">Congrats!</p><p class="winner-text">You finished in ${timerResult}!<p class="winner-text">You needed ${counter} moves</p><p class="winner-text">For this you get ${star} ${starsIcon}</p>`
-  );
-  clearInterval(interval);
-  closeModal();
-  playAgain();
-}
-// for the close Icon in the winner Modal
-function closeModal() {
-  close.click(function(e) {
-    gameEnd.removeClass("show");
-    start();
-  });
-}
-// for the playAgain button in the winner modal
-function playAgain() {
-  againButton.click(function(e) {
-    gameEnd.removeClass("show");
-    start();
-  });
-}
+    gameEnd.addClass('show');
+    const timerResult = timer.text();
+    $('.winner-message').html(
+      `<p class="winner-title">Congrats!</p><p class="winner-text">You finished in ${timerResult}!<p class="winner-text">You needed ${counter} moves</p><p class="winner-text">For this you get ${star} stars.</p>`
+    );
+    clearInterval(interval);
+    closeModal();
+    playAgain();
+  }
+  // for the close Icon in the winner Modal
+    function closeModal(){
+      close.click(function(e){
+        gameEnd.removeClass('show');
+        start();
+      });
+    }
+    // for the playAgain button in the winner modal
+    function playAgain(){
+      againButton.click(function(e){
+        gameEnd.removeClass('show');
+        start();
+      });
+    }
 
 // @description: shuffles cards (provided)
 // Shuffle function from http://stackoverflow.com/a/2450976
@@ -208,8 +197,8 @@ function shuffle(array) {
 card.click(function() {
   check(this);
   moves.text(counter);
-  if (matchCards === 8) {
+  if(matchCards == 8){
     winner();
-  }
-});
+  }});
+
 start();
